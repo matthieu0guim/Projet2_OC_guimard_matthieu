b0f46cd02437b8ba97869679fcb54c9d725a5821
Matthieu Guimard <matthieuguimard@hotmail.com>
2021-01-28 11:00:47 +0100

    premier code permettant de récupérer les infos essentielles demandées

--- ninja.py
+++ ninja.py
@@ -1,6 +1,8 @@
 import requests
-import beautifulsoup4
-
-r = requests.get("http://example.webscraping.com/")
-
-print(r.status)
+from bs4 import BeautifulSoup
+url = "http://books.toscrape.com/catalogue/olio_984/index.html"
+r = requests.get(url)
+title = BeautifulSoup(r.text, 'html.parser').find('title')
+print(title.text)
+infos = BeautifulSoup(r.text, 'html.parser').findAll('td')
+[print(str(info.text) + '\n\n') for info in infos]

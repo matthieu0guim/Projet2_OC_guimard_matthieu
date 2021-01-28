c40460304ecd260adbe39120925d7f7453377f40
Matthieu Guimard <matthieuguimard@hotmail.com>
2021-01-28 16:47:04 +0100

    version ou toutes les info demandees sur une livre sont fournis de maniere bourinne

--- ninja.py
+++ ninja.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-url = "http://books.toscrape.com/catalogue/olio_984/index.html"
-r = requests.get(url)
+"""
+ce code a seulement pour but de recueillir de manière grossière les infos qui nous intéressent
+sur la page web. La partie csv sera abordée dans un autre programme.
+On repère le nom de l'info avec les balises <th> et le contenue de l'info avec les balises <tr>.
+On stocke chaque fois dans une liste. On affiche la liste.
+"""
+
+url = "http://books.toscrape.com/catalogue/olio_984/index.html" # on donne l'url à une variable
+r = requests.get(url) 
 title = BeautifulSoup(r.text, 'html.parser').find('title')
-print(title.text)
+print('titre:',title.text[0:10])
+noms = BeautifulSoup(r.text, 'html.parser').findAll('th')
 infos = BeautifulSoup(r.text, 'html.parser').findAll('td')
-[print(str(info.text) + '\n\n') for info in infos]
+
+[print(str(nom.text), ':', str(info.text) + '\n\n') for nom in noms for info in infos]

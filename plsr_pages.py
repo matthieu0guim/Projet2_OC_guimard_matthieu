074dd1876f637d8a9ef92a3fe34ecacfda53f3e3
matt <matthieuguimard@hotmail.com>
2021-02-03 16:25:55 +0100

    ajout du fichier plsr_pages.py qui permet de chercher et stocker tous les liens des livres qui sont présents sur une page.

---
+++ plsr_pages.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+welcome_url = "http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
+r = requests.get(welcome_url)
+page = BeautifulSoup(r.text, 'html.parser')
+book_number = page.find('form', {'class' : 'form-horizontal'}).find('strong').text #on identifie le nombre de livres dans la categorie
+balises = page.find('section').findAll('h3')
+liens = []
+urls = []
+
+for i in balises:
+    liens.append(i.find('a').get('href').split('/')[-2:])
+
+for i in liens:
+    urls.append("http://books.toscrape.com/catalogue/" + i[0] + "/" + i[1])
+
+"""cette fonction renvoie simplement l'élément dune lioste correspondant à l'indice demandé. La seul particularité est"""
+def lien(liste,i):
+    if i > 0:
+        return liste[i-1]
+
+n = 0
+dico_livres = {}
+while n < len(liens):
+    n += 1
+    dico_livres['livre ' + str(n)] = lien(urls, n)
+
+    print(dico_livres)

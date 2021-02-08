d9434f9a78d8b5b4e2b83b3d5f48192e24ba4fa6
Matthieu Guimard <matthieuguimard@hotmail.com>
2021-02-08 20:22:12 +0100

    j'ai créé une fonction find_data pour suivre les indications de mouhamed. Elle fait la même chose que pour la version précédente mais en passant par une fonction. Il reste à faire de même pour l'écriture dans le fichier csv. Il faut encore créer une variable global pour avoir accès aux infos du formalisme du script

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # Titre de projet
 
 ## Description
+This programme has been redacted for web scraping purpose. It is focused
+for the website book.toscrape.com. It will load the html code of a given category.
+Then, the programme will open and parse the webpage corresponding to each book in 
+the category. Once done it will load the following informations:
+-product_page_url
+
+-universal_product_code
+
+-title
+
+-price_including_taxes
+
+-price_excluding_taxes
+
+-number_available
+
+-product_description
+
+-category
+
+-review_rating
+
+-image_url
+All these informations will be loaded in a .csv file.
+The script will ask how many categories the user want to investigate.
+Then it will ask to name them. Once done, the script will load the previous informations in the same .csv file.
 
 
 ## Execution
 ### démarrer environnement virtuel 
+The first step is to create a virtual environment in order to install the required libraries.
+
+1- create your environment with the command "py -m venv env"
+
+2- activate it with the command "env\script\activate"
+
+3- install the needed libraries with the command "pip install -r requirements.txt"
+
 ### éxécuter le script
 ```sh
 py plsr_pages.py 
--- plsr_pages.py
+++ plsr_pages.py
@@ -1,7 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 
-welcome_url = "http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
+"""cette fonction renvoie simplement l'élément dune liste correspondant à l'indice demandé. La seul particularité est"""
+
+
+def lien(liste, i):
+    if i > 0:
+        return liste[i - 1]
+
+def find_data(v):
+    dictionary = {}
+
+    dictionary['product_page_url'] = v  # on donne l'url à une variable
+    response = requests.get(dictionary['product_page_url'])  # méthode de requests qui tire de l'information depuis l'url
+    page = BeautifulSoup(response.text, 'html.parser')  # on extrait le texte qu'il y a dans la variable r
+    dictionary["title"] = page.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text
+    core = page.find('article', {'class': 'product_page'}).find_next('p').find_next('p').find_next('p').find_next('p')
+    noms = page.findAll('th')
+    dictionary["category"] = page.find('a', {'href': '../category/books/poetry_23/index.html'}).text
+    item = page.find('div', {'class': 'col-sm-6 product_main'}).findAll({'p'})
+    dictionary["review_rating"] = item[2].get('class')[1]
+    photo = page.find('div', {'class': 'item active'})
+    dictionary["image_url"] = photo.img.get("src")
+    infos = page.find('table', {'class': 'table table-striped'})
+    dictionary["product_description"] = core.text.replace(';', ',')
+    for n, i in enumerate(infos):
+
+        if n == 1:
+            dictionary["upc"] = i.find('td').text
+
+        elif n == 5:
+            dictionary["price_excluding_taxes"] = i.find('td').text
+
+        elif n == 7:
+            dictionary["price_including_taxes"] = i.find('td').text
+
+        elif n == 11:
+            dictionary["number_available"] = i.find('td').text.replace('(', '').split()[2]
+    return dictionary
+
+welcome_url = f"http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
 r = requests.get(welcome_url)
 page = BeautifulSoup(r.text, 'html.parser')
 book_number = page.find('form', {'class': 'form-horizontal'}).find(
@@ -16,14 +54,9 @@ for i in balises:
 
 for i in liens:
     urls.append(f"http://books.toscrape.com/catalogue/{i[TITRE_LIVRE]}/{i[INDEX_PAGE]}")
-print(urls)
-
-"""cette fonction renvoie simplement l'élément dune liste correspondant à l'indice demandé. La seul particularité est"""
 
 
-def lien(liste, i):
-    if i > 0:
-        return liste[i - 1]
+
 
 
 n = 0
@@ -32,57 +65,21 @@ while n < len(liens):
     n += 1
     dico_livres['livre ' + str(n)] = lien(urls, n)
 
-    # print(dico_livres)
+
 i = 0
 
 with open('page_produit.csv', 'w', encoding='utf8') as outf:
-    outf.write('product_page_url, universal_product_code, title, price_including_taxes, price_excluding_taxes, '
-               'number_available, product_description, category, review_rating, image_url\n')
+    outf.write(f"product_page_url; universal_product_code; title; price_including_taxes; price_excluding_taxes;"
+               f"number_available; product_description; category; review_rating; image_url\n")
+
 for k, v in dico_livres.items():
 
-    product_description = ""
-    product_page_url = v  # on donne l'url à une variable
-    r = requests.get(product_page_url)  # méthode de requests qui tire de l'information depuis l'url
-    page = BeautifulSoup(r.text, 'html.parser')  # on extrait le texte qu'il y a dans la variable r
-    title = page.find('div', {'class': 'col-sm-6 product_main'}).find('h1')
-    core = list(page.find_all('p'))
-    noms = page.findAll('th')
-    category = page.find('a', {'href': '../category/books/poetry_23/index.html'})
-    item = page.find('div', {'class': 'col-sm-6 product_main'}).findAll({'p'})
-    review_rating = item[2].get('class')[1]
-    photo = page.find('div', {'class': 'item active'})
-    image_url = photo.img.get("src")
-    infos = page.find('table', {'class': 'table table-striped'})
-
-    """
-           Dans cette boucle for j'ai préalablement affiché la valeur de i pour chaque valeur de n.
-           Lorsque cette valeur correspondait à l'info que je cherchais j'ai attribué la valeur à la variable correspondante.
-           Cette méthode est empirique et est donc par définition vulnérable devant un cas particulier comme par exemple un 
-           ordre des infos différents
-        """
-    for n, i in enumerate(infos):
-
-        if n == 1:
-            upc = i.find('td')
-
-        elif n == 5:
-            price_excluding_taxes = i.find('td')
-
-        elif n == 7:
-            price_including_taxes = i.find('td')
-
-        elif n == 11:
-            number_available = i.find('td').text.replace('(', '').split()[2]
-
-    for c in core:
-        if len(c.text) > 200:
-            product_description = c.text
-            break
+    dictionary = find_data(v)
 
     with open('page_produit.csv', 'a', encoding='utf8') as outf:
-
+        value = 2
         outf.write(
-            product_page_url + ',' + upc.text + ',' + title.text + ',' + price_including_taxes.text[1:] + ',' +
-            price_excluding_taxes.text[
-            1:] + ',' + number_available + ',' + product_description + ',' + category.text
-            + ',' + review_rating + ',' + image_url + '\n')
+            dictionary["product_page_url"] + ';' + dictionary["upc"] + ';' + dictionary["title"] + ';' +
+            dictionary["price_including_taxes"][value:] + ';' + dictionary["price_excluding_taxes"][value:] + ';' +
+            dictionary["number_available"] + ';' + dictionary["product_description"] + ';' + dictionary["category"] +
+            ';' + dictionary["review_rating"] + ';' + dictionary["image_url"] + "\n")

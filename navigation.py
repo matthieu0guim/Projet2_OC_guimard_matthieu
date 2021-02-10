e4b2052ab15d1ba71c03b8b3d1796ba1fec4da50
Matthieu Guimard <m.guimard@solaris-bi.com>
2021-02-10 15:36:23 +0100

    A ce stade le programme navigation.py est capable de composer individuellement avec toutes les categories du site. Peut importe si les livres tiennent sur une seule page ou sur plusieurs

--- navigation.py
+++ navigation.py
@@ -1,23 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-"""cette fonction renvoie simplement l'élément dune liste correspondant à l'indice demandé. La seul particularité est"""
-
 maintenance = {'columns': 'product_page_url; universale_product_code; title; price_including_taxes; '
                           'price_excluding_taxes; '
                           'number_available; product_description; category; review_rating; image_url\n'
     , 'csv separator': ';'}
-"""This function simply return the element of liste corresponding to the asked indice"""
 
+home_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html"
+#home_url = "http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
+response = requests.get(home_url)
+html_info = BeautifulSoup(response.text, 'html.parser')
 
 def lien(liste, i):
     if i > 0:
         return liste[i - 1]
-
-
-"""the function find_data(v) will parse the html code of a product page and save the text corresponding
-to the inquired information into a variable with an explicit name. The 'v' variable is the 
-url of the product page used to open the webpage"""
 
 
 def find_data(v):
@@ -27,10 +23,11 @@ def find_data(v):
     request = requests.get(dictionary['product_page_url'])  # méthode de requests qui tire de l'information depuis l'url
     page = BeautifulSoup(request.text, 'html.parser')  # on extrait le texte qu'il y a dans la variable r
     dictionary["title"] = page.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text
-    print(dictionary["title"])
+
     core = page.find('article', {'class': 'product_page'}).find_next('p').find_next('p').find_next('p').find_next('p')
     noms = page.findAll('th')
-    dictionary["category"] = page.find('a', {'href': '../category/books/poetry_23/index.html'}).text
+    dictionary["category"] = page.find('ul', {'class': 'breadcrumb'}).find_next('li').find_next('li').find_next('li').find('a').text
+    #print(dictionary["category"])
     item = page.find('div', {'class': 'col-sm-6 product_main'}).findAll({'p'})
     dictionary["review_rating"] = item[2].get('class')[1]
     photo = page.find('div', {'class': 'item active'})
@@ -51,14 +48,6 @@ def find_data(v):
         elif n == 11:
             dictionary["number_available"] = i.find('td').text.replace('(', '').split()[2]
     return dictionary  #
-
-
-""" The function write_data(dd) will open a .csv file and write the data collected by find_data()
-The variable dd is the dictionary created with the find_data() function. As a dictionary is not
-sorted, one have to be vigilant when we call a key. This is why the function create a list with 
-the header of each column sorted."""
-
-
 def write_data(dd):
 
     with open("page_produit.csv", 'a', encoding='utf8') as outdata:
@@ -66,8 +55,6 @@ def write_data(dd):
         for v in keys:
             outdata.write(dd[v] + ';')
         outdata.write("\n")
-
-
 def initialisation():
     with open('page_produit.csv', 'w', encoding='utf8')as outf:
         keys = maintenance['columns'].replace('\n', '').replace(' ', '').split(';')
@@ -77,35 +64,74 @@ def initialisation():
             else:
                 outf.write(header + maintenance['csv separator'])
         outf.write('\n')
+def listing(u, dico, inventory, n):
+    dic = {}
+    welcome_url = f"{u}"
+    response = requests.get(welcome_url)
+    html = BeautifulSoup(response.text, 'html.parser')
 
 
-welcome_url = f"http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
-response = requests.get(welcome_url)
-html_info = BeautifulSoup(response.text, 'html.parser')
+    balises = html.find('section').findAll('h3')
+    print(balises)
+    links = []
+    for i in balises:
+        links.append(i.find('a').get('href').split('/')[-2:])
+    print(len(links))
 
-book_number = html_info.find('form', {'class': 'form-horizontal'}).find(
-    'strong').text  # we identify the number of book in the category.
-balises = html_info.find('section').findAll('h3')
+    TITRE_LIVRE = 0
+    INDEX_PAGE = 1
 
-liens = []
-urls = []
-TITRE_LIVRE = 0
-INDEX_PAGE = 1
-for i in balises:
-    liens.append(i.find('a').get('href').split('/')[-2:])
 
-for i in liens:
-    urls.append(f"http://books.toscrape.com/catalogue/{i[TITRE_LIVRE]}/{i[INDEX_PAGE]}")
+    for i in links:
+        inventory.append(f"http://books.toscrape.com/catalogue/{i[TITRE_LIVRE]}/{i[INDEX_PAGE]}")
+    print(len(inventory), inventory)
+    dic = {}
+    for n,i in enumerate(inventory):
+        dic['livre' + str(n)] = i
+    return dic
 
-n = 0
-dico_livres = {}
-while n < len(liens):
-    n += 1
-    dico_livres['livre ' + str(n)] = lien(urls, n)
+
+
 
 
 initialisation()
-for k, v in dico_livres.items():
-    product_inspection = find_data(v)
+dico_livres = {}
+liens = []
+urls = []
+n = 0
 
-    write_data(product_inspection)
+if html_info.find('li', {'class': 'current'}):
+    number_of_pages = [f"http://books.toscrape.com/catalogue/category/books/mystery_3/page-{i}.html"
+                       for i in range(1,int(html_info.find('li', {'class': 'current'}).text.strip()[-1])+1)]
+    for i, url in enumerate(number_of_pages):
+        link_dictionary = listing(url, dico_livres, urls, n)
+
+    initialisation()
+    for k, v in link_dictionary.items():
+        product_inspection = find_data(v)
+
+        write_data(product_inspection)
+
+
+else:
+    number_of_pages = [home_url]
+    for i, url in enumerate(number_of_pages):
+        link_dictionary = listing(url, dico_livres, urls, n)
+    for k, v in link_dictionary.items():
+        product_inspection = find_data(v)
+
+        write_data(product_inspection)
+
+
+
+
+
+# for n, i in enumerate(categories):
+#     with open(f"page_produit_{i}.csv", 'w', encoding='utf8') as outf:
+#         keys = maintenance['columns'].replace('\n', '').replace(' ', '').split(';')
+#         for n, header in enumerate(keys):
+#             if n == len(keys) - 1:
+#                 outf.write(header)
+#             else:
+#                 outf.write(header + maintenance['csv separator'])
+#         outf.write('\n')

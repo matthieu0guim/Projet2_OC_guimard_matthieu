9a5d68b77e29fc5894da62d0a01a72c6df565a0d
Matthieu Guimard <matthieuguimard@hotmail.com>
2021-02-17 14:22:54 +0100

    modifs suite a l'entretien de vendredi 12/02/2021

--- plusieurs_categorie.py
+++ plusieurs_categorie.py
@@ -1,27 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-maintenance = {'columns': 'product_page_url; universale_product_code; title; price_including_taxes; '
-                          'price_excluding_taxes; '
-                          'number_available; product_description; category; review_rating; image_url\n'
-    , 'csv separator': ';'}
+maintenance = {
+    'columns': 'product_page_url; universale_product_code; title; price_including_taxes; '
+               'price_excluding_taxes; '
+               'number_available; product_description; category; review_rating; image_url',
+    'csv separator': ';'
+}
 
 
-def lien(liste, i):
-    if i > 0:
-        return liste[i - 1]
+def find_book_data(url):
+    dictionary = {'product_page_url': url}
 
+    # méthode de requests qui tire de l'information depuis l'url
+    response = requests.get(url)
+    if response.ok:
+        print(url, "[ok]")
+    else:
+        print(url, "[ko]")
 
-def find_data(v):
-    dictionary = {}
-
-    dictionary['product_page_url'] = v  # on donne l'url à une variable
-    request = requests.get(dictionary['product_page_url'])  # méthode de requests qui tire de l'information depuis l'url
-    page = BeautifulSoup(request.text, 'html.parser')  # on extrait le texte qu'il y a dans la variable r
+    # on extrait le texte qu'il y a dans la variable response
+    page = BeautifulSoup(response.text, 'html.parser')
     dictionary["title"] = page.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text
 
     core = page.find('article', {'class': 'product_page'}).find_next('p').find_next('p').find_next('p').find_next('p')
-    noms = page.findAll('th')
+
     dictionary["category"] = page.find('ul', {'class': 'breadcrumb'}).find_next('li').find_next('li').find_next(
         'li').find('a').text
     # print(dictionary["category"])
@@ -31,27 +34,27 @@ def find_data(v):
     dictionary["image_url"] = photo.img.get("src")
     infos = page.find('table', {'class': 'table table-striped'})
     dictionary["product_description"] = core.text.replace(';', ',')
-    for n, i in enumerate(infos):
+    for index, tr in enumerate(infos):
 
-        if n == 1:
-            dictionary["universale_product_code"] = i.find('td').text
+        if index == 1:
+            dictionary["universale_product_code"] = tr.find('td').text
 
-        elif n == 5:
-            dictionary["price_excluding_taxes"] = i.find('td').text[2:]
+        elif index == 5:
+            dictionary["price_excluding_taxes"] = tr.find('td').text[2:]
 
-        elif n == 7:
-            dictionary["price_including_taxes"] = i.find('td').text[2:]
+        elif index == 7:
+            dictionary["price_including_taxes"] = tr.find('td').text[2:]
 
-        elif n == 11:
-            dictionary["number_available"] = i.find('td').text.replace('(', '').split()[2]
+        elif index == 11:
+            dictionary["number_available"] = tr.find('td').text.replace('(', '').split()[2]
     return dictionary  #
 
 
-def write_data(dd, name):
+def write_data(data, name):
     with open(f"{name}", 'a', encoding='utf8') as outdata:
         keys = maintenance['columns'].replace('\n', '').replace(' ', '').split(';')
-        for v in keys:
-            outdata.write(dd[v] + ';')
+        for k in keys:
+            outdata.write(data[k] + ';')
         outdata.write("\n")
 
 
@@ -78,55 +81,60 @@ def listing(u, inventory):
     return dic
 
 
-home_url = "http://books.toscrape.com/index.html"
-response = requests.get(home_url)
-html_info = BeautifulSoup(response.text, 'html.parser')
+if __name__ == "__main__":
+    home_url = "http://books.toscrape.com/index.html"
+    response = requests.get(home_url)
+    html_info = BeautifulSoup(response.text, 'html.parser')
 
-categories = html_info.find('ul', {'class': 'nav nav-list'}).find('ul').find_all('a')
-liste = []
-for i in categories:
-    liste.append(i.text.replace('\n', '').strip())
-print(liste)
-files_list = []
-for n, i in enumerate(liste):
-    with open(f"page_produit_{i}.csv", 'w', encoding='utf8') as outf:
-        files_list.append(f"page_produit_{i}.csv")
-        keys = maintenance['columns'].replace('\n', '').replace(' ', '').split(';')
-        for n, header in enumerate(keys):
-            if n == len(keys) - 1:
-                outf.write(header)
-            else:
-                outf.write(header + maintenance['csv separator'])
-        outf.write('\n')
+    categories = html_info.find('ul', {'class': 'nav nav-list'}).find('ul').find_all('a')
 
-for nb, cat in enumerate(liste):
-    home_cat_url = f"http://books.toscrape.com/catalogue/category/books/{cat.lower().replace(' ', '-')}_{nb + 2}/index.html"
-    response = requests.get(home_cat_url)
-    html_info_cat = BeautifulSoup(response.text, 'html.parser')
+    liste = []
+    for i in categories:
+        liste.append(i.text.replace('\n', '').strip())
 
-    dico_livres = {}
-    liens = []
-    urls = []
-    if html_info_cat.find('li', {'class': 'current'}):
-        # print("home_cat_url:", home_cat_url)
-        number_of_pages = [f"{home_cat_url.replace('index.html', '')}page-{i}.html"
-                           for i in range(1, int(html_info_cat.find('li', {'class': 'current'}).text.strip()[-1]) + 1)]
-        # print("nombre de page:", number_of_pages)
-        for url in number_of_pages:
-            link_dictionary = listing(url, urls)
+    files_list = []
+    for n, i in enumerate(liste):
+        with open(f"page_produit_{i}.csv", 'w', encoding='utf8') as outf:
+            files_list.append(f"page_produit_{i}.csv")
+            keys = maintenance['columns'].split(';')
+            for n, header in enumerate(keys):
+                if n == len(keys) - 1:
+                    outf.write(header + '\n')
+                else:
+                    outf.write(header + maintenance['csv separator'])
 
-        for k, v in link_dictionary.items():
-            product_inspection = find_data(v)
-            # print(files_list[i])
+    for nb, cat in enumerate(liste):
+        home_cat_url = f"http://books.toscrape.com/catalogue/category/books/{cat.lower().replace(' ', '-')}_{nb + 2}/index.html"
+        response = requests.get(home_cat_url)
+        html_info_cat = BeautifulSoup(response.text, 'html.parser')
 
-            write_data(product_inspection, files_list[nb])
+        dico_livres = {}
+        liens = []
+        urls = []
+        if html_info_cat.find('li', {'class': 'current'}):
+            # print("home_cat_url:", home_cat_url)
+            number_of_pages = [f"{home_cat_url.replace('index.html', '')}page-{i}.html"
+                               for i in range(1, int(html_info_cat.find('li', {'class': 'current'}).text.strip()[-1]) + 1)]
+            # print("nombre de page:", number_of_pages)
+            link_dictionary = {}
+            for url in number_of_pages:
+                link_dictionary = listing(url, urls)
 
-    else:
-        number_of_pages = [home_cat_url]
-        # print(home_cat_url)
-        for url in number_of_pages:
-            link_dictionary = listing(url, urls)
-        for k, v in link_dictionary.items():
-            product_inspection = find_data(v)
+            for k, v in link_dictionary.items():
+                product_inspection = find_book_data(v)
+                # print(files_list[i])
 
-            write_data(product_inspection, files_list[nb])
+                write_data(product_inspection, files_list[nb])
+            print("if ok")
+        else:
+            number_of_pages = [home_cat_url]
+            # print(home_cat_url)
+            for url in number_of_pages:
+
+                link_dictionary = listing(url, urls)
+            print("else ok")
+            for k, v in link_dictionary.items():
+                product_inspection = find_book_data(v)
+
+                write_data(product_inspection, files_list[nb])
+
